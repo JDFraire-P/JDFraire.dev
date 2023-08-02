832696df65647f3ed782b2c199a5bd401b1055a0
Daniel Fraire <jdfraire.p@gmail.com>
2023-08-02 02:44:31 -0700

    Mi page

---
+++ public/script.js
@@ -0,0 +1,7 @@
+// 
+// document.addEventListener("DOMContentLoaded", function() {
+//     const header = document.querySelector("header");
+//     header.addEventListener("click", function() {
+//         header.style.backgroundColor = "lightblue";
+//     });
+// });
